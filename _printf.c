c21cb76a22dcc5d7d06237a9581f86ad451c759d
MogammedRa-Eez <zardad2603@gmail.com>
2023-11-17 12:38:46 -0800

    UPDATE

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 {
 	match m[] = {
 		{"%c", printf_char}, {"%s", printf_string},
-		{"%%", print_37}, {"%d", print_dec}, {"%i", print_int}, {"%r", print_revs},
+		{"%%", printf_37}, {"%d", print_dec}, {"%i", print_int}, {"%r", print_revs},
 		{"%R", print_rot13}, {"%b", print_bin}, {"%u", print_unsigned},
 		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
 		{"%S", print_exc_string}, {"%p", print_pointer}
