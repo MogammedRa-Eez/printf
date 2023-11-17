d8d65f78b89eb0f4d7dc2faf4b4889deb7236b9e
MogammedRa-Eez <zardad2603@gmail.com>
2023-11-17 12:36:42 -0800

    UPDATE

--- main.h
+++ main.h
@@ -27,7 +27,7 @@ int _putchar(char c);
 int printf_string(va_list val);
 int _strlen(char *str);
 int _strlenc(const char *str);
-int print_37(void);
+int printf_37(void);
 int print_int(va_list args);
 int print_dec(va_list args);
 int print_bin(va_list val);
--- print_37.c
+++ print_37.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_37 - prints char 37
+ * printf_37 - prints char 37
  * Return: 1
  */
 
-int print_37(void)
+int printf_37(void)
 {
 	_putchar(37);
 	return (1);
