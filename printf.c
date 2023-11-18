9d5886c420255b8b1f58afeef75ade1cb2a9882b
MogammedRa-Eez <zardad2603@gmail.com>
2023-11-18 06:35:56 -0800

    printf

---
+++ printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _printf - function that selects correct function to print
+ * @format: identifier look for
+ * Return: length of string
+*/
+
+int _printf(const char *format, ...)
+{
+	match m[] = {
+		{"%c", printf_char}, {"%s", printf_string},
+		{"%%", printf_37}, {"%d", print_dec}, {"%i", print_int}, {"%r", print_revs},
+		{"%R", print_rot13}, {"%b", print_bin}, {"%u", print_unsigned},
+		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_exc_string}, {"%p", print_pointer}
+	};
+
+	va_list args;
+	int i = 0, len = 0;
+	int j;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	while (format[i] != '\0')
+	{
+Here:		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len = len + m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		i++;
+		len++;
+	}
+	va_end(args);
+	return (len);
+}
