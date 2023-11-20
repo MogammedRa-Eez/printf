11a952f11a3bdf26dc6d0f78cca2e46c076471b7
MogammedRa-Eez <zardad2603@gmail.com>
2023-11-19 23:22:25 -0800

    UPDATE

--- print_exc_string.c
+++ print_exc_string.c
@@ -8,39 +8,34 @@
 
 int print_exc_string(va_list val)
 {
-	int i;
 	char *s;
-	int k, j = 0;
-	char c = 'A';
-	char d[1];
+	int i, len = 0;
+	char c = 'A' - ':';
+	char value;
 
 	s = va_arg(val, char *);
 	if (s == NULL)
 		s = "(null)";
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] < 32 || s[i] > 127)
+		if (s[i] < 32 || s[i] >= 127)
 		{
 			_putchar('\\');
 			_putchar('x');
-			j = j + 2;
-			d[0] = s[i] / 16;
-			d[1] = s[i] % 16;
-			for (k = 0; k < 2; k++)
+			len = len + 2;
+			value = s[i];
+			if (value < 16)
 			{
-				if (d[k] >= 9)
-
-					_putchar('0' + c + d[k]);
-				else
-					_putchar('0' + d[k]);
+				_putchar('0' + c + value);
+				len++;
 			}
-			j = j + print_HEX_extra(d[k]);
+			len = len + print_HEX_extra(value);
 		}
 		else
 		{
 			_putchar(s[i]);
-			j++;
+			len++;
 		}
 	}
-	return (j);
+	return (len);
 }
