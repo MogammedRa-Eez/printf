ea18b119376678a00ed840ca15b21d06ea1deb6d
MogammedRa-Eez <zardad2603@gmail.com>
2023-11-19 22:57:10 -0800

    UPDATE

--- print_exc_string.c
+++ print_exc_string.c
@@ -2,38 +2,39 @@
 
 /**
  * print_exc_string - print exclusive string.
- * @val: string to print
- * Return: number of chars to be printed
+ * @val: arguments
+ * Return: length of the string
  */
 
 int print_exc_string(va_list val)
 {
-	unsigned int i;
-	char *s = va_arg(val, char *);
+	int i;
+	char *s;
 	int k, j = 0;
-	char c = 'A' - ':';
-	char d[2];
+	char c = 'A';
+	char d[1];
 
+	s = va_arg(val, char *);
 	if (s == NULL)
 		s = "(null)";
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] < 32 || s[i] >= 127)
+		if (s[i] < 32 || s[i] > 127)
 		{
 			_putchar('\\');
 			_putchar('x');
-			j += 2;
+			j = j + 2;
 			d[0] = s[i] / 16;
 			d[1] = s[i] % 16;
 			for (k = 0; k < 2; k++)
 			{
-				if (d[k] >= 10)
+				if (d[k] >= 9)
 
 					_putchar('0' + c + d[k]);
 				else
 					_putchar('0' + d[k]);
 			}
-			j += print_HEX_extra(d[k]);
+			j = j + print_HEX_extra(d[k]);
 		}
 		else
 		{
